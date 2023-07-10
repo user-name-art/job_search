88af94a0c5a17c9caa1a0658cee2c288c389f8ac
Art Gilyazov <art.gilyazov@mail.ru>
2023-07-10 16:23:36 +0300

    add module parsers

--- webapp/parsers/habr.py
+++ webapp/parsers/habr.py
@@ -10,16 +10,7 @@ from dotenv import load_dotenv
 
 from webapp.models import Vacancy
 from webapp.db import db_session
-
-
-def get_html(url):
-    try:
-        result = requests.get(url)
-        result.raise_for_status()
-        return result.text
-    except(requests.RequestException):
-        print('Network error.')
-        return False
+from webapp.parsers.utils import save_data_to_db, get_html
 
 
 def get_habr_vacancies(html):
@@ -44,20 +35,6 @@ def get_habr_vacancies(html):
     save_data_to_db(result_vacancies)
     
     return result_vacancies
-
-
-def save_data_to_db(all_vacancies):
-    for vacancy in all_vacancies:
-        vacancy_exists = Vacancy.query.filter(Vacancy.url == vacancy['url']).count()
-        
-        if not vacancy_exists:
-            entity = Vacancy(company=vacancy['company'], vacancy_title=vacancy['vacancy_title'], 
-                        url=vacancy['url'], published=vacancy['published'])
-            db_session.add(entity)
-            db_session.commit()
-            print(f"Вакансия {vacancy['vacancy_title']} компании {vacancy['company']} добавлена.")
-        else:
-            print(f"Вакансия {vacancy['vacancy_title']} компании {vacancy['company']} уже есть в базе.")
     
 
 if __name__ == '__main__':
---
+++ webapp/parsers/utils.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import requests
+import time
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+from webapp.models import Vacancy
+from webapp.db import db_session
+
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException):
+        print('Network error.')
+        return False
+
+def save_data_to_db(all_vacancies):
+    for vacancy in all_vacancies:
+        vacancy_exists = Vacancy.query.filter(Vacancy.url == vacancy['url']).count()
+        
+        if not vacancy_exists:
+            entity = Vacancy(company=vacancy['company'], vacancy_title=vacancy['vacancy_title'], 
+                        url=vacancy['url'], published=vacancy['published'])
+            db_session.add(entity)
+            db_session.commit()
+            print(f"Вакансия {vacancy['vacancy_title']} компании {vacancy['company']} добавлена.")
+        else:
+            print(f"Вакансия {vacancy['vacancy_title']} компании {vacancy['company']} уже есть в базе.")
