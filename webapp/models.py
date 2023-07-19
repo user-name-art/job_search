00a67746be5482034809044df95eb031ac33097f
Art Gilyazov <art.gilyazov@mail.ru>
2023-07-19 22:47:15 +0300

    fix repr for class Vacancy

--- webapp/models.py
+++ webapp/models.py
@@ -19,7 +19,7 @@ class Vacancy(Base):
     source = Column(String)
 
     def __repr__(self):
-        return f'Vacancy {self.id}, {self.company}, {self.vacancy_title}'
+        return f'{self.__class__.__name__} ({self.id}, {self.company}, {self.vacancy_title})'
 
 
 if __name__ == '__main__':
